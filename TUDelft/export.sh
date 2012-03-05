5c5e7d09f4a55631b4c634aa66bf3b526399b943
Christophe De Wagter <dewagter@gmail.com>
2012-03-05 15:19:44 +0100

    one step closer to paparazzi master

--- TUDelft/export.sh
+++ TUDelft/export.sh
@@ -27,6 +27,8 @@ chmod +x ./rm.sh
 rm ./rm.sh
 rm -rf ./sw/airborne/modules/onboardcam
 rm -rf ./sw/airborne/modules/opticflow
+rm -rf ./conf/conf.xml
+rm -rf ./conf/controlpanel.xml
 #rm -rf ./sw/airborne/boards/tiny_sense.h
 #rm -rf ./sw/airborne/booz/aerovinci
 #rm -rf ./conf/joysticks
