9427ecefdbb042b516e1a7cefe9ed96d8f0e55ed
MavLab <microuav@gmail.com>
2012-05-12 22:58:49 +0200

    Last Attempt Of Nr 2

---
+++ checklist.sh
@@ -0,0 +1,7 @@
+spd-say "Checklist, press enter to continue"
+read
+spd-say "Check Battery Connected"
+read
+spd-say "Check Telemetry"
+read
+spd-say "Check Safety Pilot is Sober and has sufficient Rest"
