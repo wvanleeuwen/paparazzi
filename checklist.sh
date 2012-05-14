2af2077409512bd219f1e9dd367888068bbf090d
MavLab <microuav@gmail.com>
2012-05-14 22:07:59 +0200

    UAVforge attempt 1 failed - lost data link

--- checklist.sh
+++ checklist.sh
@@ -10,8 +10,12 @@ spd-say "Check GPS is Present"
 read
 spd-say "Check Pitot Tube Present"
 read
+spd-say "Check Battery Connector Fits Tight And not Too Loose"
+read
 spd-say "Check Battery Connected"
 read
 spd-say "Check Telemetry"
 read
 spd-say "Check Safety Pilot is Sober and has sufficient Rest"
+read
+spd-say "Saftey Pilot Not Sane, but that is OK since we will be flying full autonomous"
