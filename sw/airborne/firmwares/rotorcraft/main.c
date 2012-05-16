5b2cc8a6d1811167925684b1bb3b8e6767397b72
Pranay Sinha <psinha@psinha-laptop.(none)>
2012-05-15 22:49:24 -0700

    Increased startup delay in main.c since there are still some occassions where Asp2.1 does not init properly due to boot issues.

--- sw/airborne/firmwares/rotorcraft/main.c
+++ sw/airborne/firmwares/rotorcraft/main.c
@@ -88,7 +88,7 @@ STATIC_INLINE void main_init( void ) {
 //#ifndef NO_FUCKING_STARTUP_DELAY
 //#ifndef RADIO_CONTROL_SPEKTRUM_PRIMARY_PORT
   /* IF THIS IS NEEDED SOME PERHIPHERAL THEN PLEASE MOVE IT THERE */
-  for (uint32_t startup_counter=0; startup_counter<2000000; startup_counter++){
+  for (uint32_t startup_counter=0; startup_counter<4000000; startup_counter++){
     __asm("nop");
   }
 //#endif
