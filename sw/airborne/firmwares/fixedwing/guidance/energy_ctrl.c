850f1cd4ff9ede0b161fa3d8bd26f8d4c6c3875b
Christophe De Wagter <dewagter@gmail.com>
2012-07-03 06:04:43 +0200

    [TE-CTRL] Empty Default Controller

---
+++ sw/airborne/firmwares/fixedwing/guidance/energy_ctrl.c
@@ -0,0 +1,169 @@
+/*
+ * Copyright (C) 2006  Pascal Brisset, Antoine Drouin, Michel Gorraz
+ *
+ * This file is part of paparazzi.
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+/**
+ *  @file firmwares/fixedwing/guidance/guidance_v.c
+ *  Vertical control using total energy control for fixed wing vehicles.
+ *
+
+
+	=================================================
+	Energy:
+	------
+	E 		= mgh + 1/2mV^2
+	Edot / V 	= (gamma + Vdot/g) * W
+
+	equilibrium
+
+	Vdot / g = Thrust/W - Drag/W - sin(gamma)
+	with: Drag/Weight = (Cl/Cd)^-1
+
+	-glide angle: Vdot = 0, T=0 ==> gamma = Cd/Cl
+	-level flight: Vdot = 0, gamma=0 ==> W/T = Cl/Cd
+	=================================================
+	
+	Strategy:  thrust = path + acceleration[g] (total energy)
+		   pitch = path - acceleration[g]  (energy balance)
+
+	Pseudo-Control Unit = dimensionless acceleration [g]
+
+		- pitch <-> pseudocontrol:    sin(Theta) steers Vdot in [g]
+		- throttle <-> pseudocontrol: motor characteristic as function of V x throttle steeds VDot
+
+ */
+
+#pragma message "CAUTION! Using TOTAL ENERGY CONTROLLER: Experimental!"
+
+#include "firmwares/fixedwing/guidance/energy_ctrl.h"
+#include "estimator.h"
+#include "subsystems/nav.h"
+#include "generated/airframe.h"
+#include "firmwares/fixedwing/autopilot.h"
+
+/////// DEFAULT GUIDANCE_V NECESSITIES //////
+
+/* mode */
+uint8_t v_ctl_mode = V_CTL_MODE_MANUAL;
+uint8_t v_ctl_climb_mode = V_CTL_CLIMB_MODE_AUTO_ENERGY;
+uint8_t v_ctl_auto_throttle_submode = V_CTL_CLIMB_MODE_AUTO_ENERGY;
+float v_ctl_auto_throttle_sum_err = 0;
+
+#ifdef LOITER_TRIM
+#error "Energy Controller can not accep Loiter Trim"
+#endif
+
+/////// ACTUALLY USED STUFF //////
+
+/* outer loop */
+float v_ctl_altitude_setpoint;
+float v_ctl_altitude_pre_climb; ///< Path Angle
+float v_ctl_altitude_pgain;
+float v_ctl_altitude_error;    ///< in meters, (setpoint - alt) -> positive = too low
+
+/* inner loop */
+float v_ctl_climb_setpoint;
+
+/* "auto throttle" inner loop parameters */
+float v_ctl_auto_throttle_nominal_cruise_throttle;
+float v_ctl_auto_throttle_climb_throttle_increment;
+float v_ctl_auto_throttle_pitch_of_vz_pgain;
+
+pprz_t v_ctl_throttle_setpoint;
+pprz_t v_ctl_throttle_slewed;
+
+float v_ctl_auto_airspeed_setpoint;
+
+void v_ctl_init( void ) {
+  /* mode */
+  v_ctl_mode = V_CTL_MODE_MANUAL;
+
+  /* outer loop */
+  v_ctl_altitude_setpoint = 0.;
+  v_ctl_altitude_pgain = V_CTL_ALTITUDE_PGAIN;
+
+  /* inner loops */
+  v_ctl_climb_setpoint = 0.;
+
+  /* "auto throttle" inner loop parameters */
+  v_ctl_auto_throttle_nominal_cruise_throttle = V_CTL_AUTO_THROTTLE_NOMINAL_CRUISE_THROTTLE;
+  v_ctl_auto_throttle_climb_throttle_increment = V_CTL_AUTO_THROTTLE_CLIMB_THROTTLE_INCREMENT;
+  v_ctl_auto_throttle_pitch_of_vz_pgain = V_CTL_AUTO_THROTTLE_PITCH_OF_VZ_PGAIN;
+
+  v_ctl_auto_airspeed_setpoint = NOMINAL_AIRSPEED;
+
+  v_ctl_throttle_setpoint = 0;
+}
+
+/**
+ * outer loop
+ * \brief Computes v_ctl_climb_setpoint and sets v_ctl_auto_throttle_submode
+ */
+void v_ctl_altitude_loop( void ) 
+{
+  v_ctl_altitude_error = v_ctl_altitude_setpoint - estimator_z;
+  v_ctl_climb_setpoint = v_ctl_altitude_pgain * v_ctl_altitude_error;
+  BoundAbs(v_ctl_climb_setpoint, V_CTL_ALTITUDE_MAX_CLIMB);
+}
+
+
+/**
+ * auto throttle inner loop
+ * \brief
+ */
+
+void v_ctl_climb_loop( void ) 
+{
+  static float last_err;
+
+  float f_throttle = 0;
+  float err  = estimator_z_dot - v_ctl_climb_setpoint;
+  float d_err = err - last_err;
+  last_err = err;
+  float controlled_throttle = v_ctl_auto_throttle_nominal_cruise_throttle
+    + v_ctl_auto_throttle_climb_throttle_increment * v_ctl_climb_setpoint;
+
+  /* pitch pre-command */
+  float v_ctl_pitch_of_vz = (v_ctl_climb_setpoint /*+ d_err * v_ctl_auto_throttle_pitch_of_vz_dgain*/) * v_ctl_auto_throttle_pitch_of_vz_pgain;
+
+
+    f_throttle = controlled_throttle;
+    nav_pitch += v_ctl_pitch_of_vz;
+
+  v_ctl_throttle_setpoint = TRIM_UPPRZ(f_throttle * MAX_PPRZ);
+}
+
+
+#ifdef V_CTL_THROTTLE_SLEW_LIMITER
+#define V_CTL_THROTTLE_SLEW (1./CONTROL_RATE/(V_CTL_THROTTLE_SLEW_LIMITER))
+#endif
+
+#ifndef V_CTL_THROTTLE_SLEW
+#define V_CTL_THROTTLE_SLEW 1.
+#endif
+
+/** \brief Computes slewed throttle from throttle setpoint
+    called at 20Hz
+ */
+void v_ctl_throttle_slew( void ) {
+  pprz_t diff_throttle = v_ctl_throttle_setpoint - v_ctl_throttle_slewed;
+  BoundAbs(diff_throttle, TRIM_PPRZ(V_CTL_THROTTLE_SLEW*MAX_PPRZ));
+  v_ctl_throttle_slewed += diff_throttle;
+}
---
+++ sw/airborne/firmwares/fixedwing/guidance/energy_ctrl.h
@@ -0,0 +1,93 @@
+/*
+ * Copyright (C) 2012 TUDelft
+ *
+ * This file is part of paparazzi.
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+/**
+ * @file firmwares/fixedwing/guidance/energy_ctrl.h
+ * Vertical control using total energy control for fixed wing vehicles.
+ *
+ */
+
+#ifndef FW_V_CTL_H
+#define FW_V_CTL_H
+
+#include <inttypes.h>
+#include "paparazzi.h"
+
+/////// DEFAULT GUIDANCE_V NECESSITIES //////
+
+/* Vertical mode */
+#define V_CTL_MODE_MANUAL        0
+#define V_CTL_MODE_AUTO_THROTTLE 1
+#define V_CTL_MODE_AUTO_CLIMB    2
+#define V_CTL_MODE_AUTO_ALT      3
+#define V_CTL_MODE_NB            4
+extern uint8_t v_ctl_mode;
+
+extern uint8_t v_ctl_climb_mode;
+extern uint8_t v_ctl_auto_throttle_submode;
+
+#define V_CTL_CLIMB_MODE_AUTO_ENERGY 	   0
+#define V_CTL_CLIMB_MODE_AUTO_PITCHONLY    1
+
+// Needed for telemetry
+extern float v_ctl_auto_throttle_sum_err;
+
+// Needed for course loop gain 
+extern float v_ctl_altitude_error;    ///< in meters, (setpoint - alt) -> positive = too low
+
+/////// ACTUALLY USED STUFF //////
+
+/* outer loop */
+// extern float v_ctl_altitude_error;    ///< in meters, (setpoint - alt) -> positive = too low
+extern float v_ctl_altitude_setpoint; ///< in meters above MSL
+extern float v_ctl_altitude_pre_climb; ///< Path Angle
+extern float v_ctl_altitude_pgain;
+//extern float v_ctl_altitude_max_climb;
+
+/* "auto throttle" inner loop parameters */
+extern float v_ctl_auto_throttle_nominal_cruise_throttle;
+extern float v_ctl_auto_throttle_climb_throttle_increment;
+extern float v_ctl_auto_throttle_pitch_of_vz_pgain;
+
+extern pprz_t v_ctl_throttle_setpoint;
+extern pprz_t v_ctl_throttle_slewed;
+
+extern void v_ctl_init( void );
+extern void v_ctl_altitude_loop( void );
+extern void v_ctl_climb_loop ( void );
+
+/* "airspeed" inner loop parameters */
+extern float v_ctl_auto_airspeed_setpoint;
+
+/** Computes throttle_slewed from throttle_setpoint */
+extern void v_ctl_throttle_slew( void );
+
+#define guidance_v_SetCruiseThrottle(_v) { \
+  v_ctl_auto_throttle_cruise_throttle = (_v ? _v : v_ctl_auto_throttle_nominal_cruise_throttle); \
+  Bound(v_ctl_auto_throttle_cruise_throttle, v_ctl_auto_throttle_min_cruise_throttle, v_ctl_auto_throttle_max_cruise_throttle); \
+}
+
+#define guidance_v_SetAutoThrottleIgain(_v) {	\
+    v_ctl_auto_throttle_igain = _v;		\
+    v_ctl_auto_throttle_sum_err = 0;		\
+  }
+
+#endif /* FW_V_CTL_H */
