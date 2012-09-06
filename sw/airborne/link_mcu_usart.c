f8ddeea8e0cebffd33db5a2ee3dc469bf8fce8ac
Christophe De Wagter <dewagter@gmail.com>
2012-09-06 17:02:28 +0200

    InterMcu Uart

--- sw/airborne/link_mcu_usart.c
+++ sw/airborne/link_mcu_usart.c
@@ -22,18 +22,207 @@
  *
  */
 
+#define USE_UART0
+
 #include "link_mcu_usart.h"
-#include "uart.h"
-#include "ubx_protocol.h"
-#include "gps_ubx.h"
-#include "gps.h"
+#include "mcu_periph/uart.h"
+
 #include "commands.h"
 
-void link_mcu_init( void )
-{
-  uart0_init();
+//////////////////////////////////////////////////////////////////////////////////////////////
+// LINK
+
+#define INTERMCU_LINK Uart0
+
+#define __InterMcuLink(dev, _x) dev##_x
+#define _InterMcuLink(dev, _x)  __InterMcuLink(dev, _x)
+#define InterMcuLink(_x) _InterMcuLink(INTERMCU_LINK, _x)
+
+#define InterMcuBuffer() InterMcuLink(ChAvailable())
+
+#define InterMcuUartSend1(c) InterMcuLink(Transmit(c))
+#define InterMcuUartSetBaudrate(_a) InterMcuLink(SetBaudrate(_a))
+#define InterMcuUartRunning InterMcuLink(TxRunning)
+#define InterMcuUartSendMessage InterMcuLink(SendMessage)
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// PROTOCOL
+
+#define INTERMCU_SYNC1 0xB5
+#define INTERMCU_SYNC2 0x62
+
+#define InterMcuInitCheksum() { intermcu_data.send_ck_a = intermcu_data.send_ck_b = 0; }
+#define UpdateChecksum(c) { intermcu_data.send_ck_a += c; intermcu_data.send_ck_b += intermcu_data.send_ck_a; }
+#define InterMcuTrailer() { InterMcuUartSend1(intermcu_data.send_ck_a);  InterMcuUartSend1(intermcu_data.send_ck_b); InterMcuUartSendMessage(); }
+
+#define InterMcuSend1(c) { uint8_t i8=c; InterMcuUartSend1(i8); UpdateChecksum(i8); }
+#define InterMcuSend2(c) { uint16_t i16=c; InterMcuSend1(i16&0xff); InterMcuSend1(i16 >> 8); }
+#define InterMcuSend1ByAddr(x) { InterMcuSend1(*x); }
+#define InterMcuSend2ByAddr(x) { InterMcuSend1(*x); InterMcuSend1(*(x+1)); }
+#define InterMcuSend4ByAddr(x) { InterMcuSend1(*x); InterMcuSend1(*(x+1)); InterMcuSend1(*(x+2)); InterMcuSend1(*(x+3)); }
+
+#define InterMcuHeader(nav_id, msg_id, len) {        \
+    InterMcuUartSend1(INTERMCU_SYNC1);                    \
+    InterMcuUartSend1(INTERMCU_SYNC2);                    \
+    InterMcuInitCheksum();                           \
+    InterMcuSend1(nav_id);                           \
+    InterMcuSend1(msg_id);                           \
+    InterMcuSend2(len);                              \
+  }
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// MESSAGES
+
+// class
+#define MSG_INTERMCU_ID 100
+
+#define MSG_INTERMCU_COMMAND_ID 0x05
+#define MSG_INTERMCU_COMMAND_S1(_intermcu_payload) (uint16_t)(*((uint8_t*)_intermcu_payload+0)|*((uint8_t*)_intermcu_payload+1+0)<<8)
+#define MSG_INTERMCU_COMMAND_S2(_intermcu_payload) (uint16_t)(*((uint8_t*)_intermcu_payload+2)|*((uint8_t*)_intermcu_payload+1+2)<<8)
+#define MSG_INTERMCU_COMMAND_S3(_intermcu_payload) (uint16_t)(*((uint8_t*)_intermcu_payload+4)|*((uint8_t*)_intermcu_payload+1+4)<<8)
+#define MSG_INTERMCU_COMMAND_S4(_intermcu_payload) (uint16_t)(*((uint8_t*)_intermcu_payload+6)|*((uint8_t*)_intermcu_payload+1+6)<<8)
+#define MSG_INTERMCU_COMMAND_S5(_intermcu_payload) (uint16_t)(*((uint8_t*)_intermcu_payload+8)|*((uint8_t*)_intermcu_payload+1+8)<<8)
+#define MSG_INTERMCU_COMMAND_S6(_intermcu_payload) (uint16_t)(*((uint8_t*)_intermcu_payload+10)|*((uint8_t*)_intermcu_payload+1+10)<<8)
+
+#define InterMcuSend_INTERMCU_COMMAND(s1,s2,s3,s4,s5,s6) { \
+  InterMcuHeader(MSG_INTERMCU_ID, MSG_INTERMCU_COMMAND_ID, 12);\
+  uint16_t _s1 = s1; InterMcuSend2ByAddr((uint8_t*)&_s1);\
+  uint16_t _s2 = s2; InterMcuSend2ByAddr((uint8_t*)&_s2);\
+  uint16_t _s3 = s3; InterMcuSend2ByAddr((uint8_t*)&_s3);\
+  uint16_t _s4 = s4; InterMcuSend2ByAddr((uint8_t*)&_s4);\
+  uint16_t _s5 = s5; InterMcuSend2ByAddr((uint8_t*)&_s5);\
+  uint16_t _s6 = s6; InterMcuSend2ByAddr((uint8_t*)&_s6);\
+  InterMcuTrailer();\
 }
 
+#define MSG_INTERMCU_FBW_ID 0x06
+#define MSG_INTERMCU_FBW_MOD(_intermcu_payload) (uint8_t)(*((uint8_t*)_intermcu_payload+0))
+#define MSG_INTERMCU_FBW_STAT(_intermcu_payload) (uint8_t)(*((uint8_t*)_intermcu_payload+1))
+#define MSG_INTERMCU_FBW_ERR(_intermcu_payload) (uint8_t)(*((uint8_t*)_intermcu_payload+2))
+#define MSG_INTERMCU_FBW_VOLT(_intermcu_payload) (uint8_t)(*((uint8_t*)_intermcu_payload+3))
+#define MSG_INTERMCU_FBW_CURRENT(_intermcu_payload) (uint16_t)(*((uint8_t*)_intermcu_payload+4)|*((uint8_t*)_intermcu_payload+1+4)<<8)
+
+#define InterMcuSend_INTERMCU_FBW(mod,stat,err,volt,current) { \
+  InterMcuHeader(MSG_INTERMCU_ID, MSG_INTERMCU_FBW_ID, 6);\
+  uint8_t _mod = mod; InterMcuSend1ByAddr((uint8_t*)&_mod);\
+  uint8_t _stat = stat; InterMcuSend1ByAddr((uint8_t*)&_stat);\
+  uint8_t _err = err; InterMcuSend1ByAddr((uint8_t*)&_err);\
+  uint8_t _volt = volt; InterMcuSend1ByAddr((uint8_t*)&_volt);\
+  uint16_t _current = current; InterMcuSend2ByAddr((uint8_t*)&_current);\
+  InterMcuTrailer();\
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// PARSER
+
+/* parser status */
+#define UNINIT        0
+#define GOT_SYNC1     1
+#define GOT_SYNC2     2
+#define GOT_CLASS     3
+#define GOT_ID        4
+#define GOT_LEN1      5
+#define GOT_LEN2      6
+#define GOT_PAYLOAD   7
+#define GOT_CHECKSUM1 8
+
+
+#define INTERMCU_MAX_PAYLOAD 255
+struct InterMcuData {
+  bool_t msg_available;
+  uint8_t msg_buf[INTERMCU_MAX_PAYLOAD] __attribute__ ((aligned));
+  uint8_t msg_id;
+  uint8_t msg_class;
+
+  uint8_t status;
+  uint16_t len;
+  uint8_t msg_idx;
+  uint8_t ck_a, ck_b;
+  uint8_t send_ck_a, send_ck_b;
+  uint8_t error_cnt;
+};
+
+struct InterMcuData intermcu_data;
+
+/* INTERMCU parsing */
+void intermcu_parse( uint8_t c );
+void intermcu_parse( uint8_t c ) {
+  if (intermcu_data.status < GOT_PAYLOAD) {
+    intermcu_data.ck_a += c;
+    intermcu_data.ck_b += intermcu_data.ck_a;
+  }
+  switch (intermcu_data.status) {
+  case UNINIT:
+    if (c == INTERMCU_SYNC1)
+      intermcu_data.status++;
+    break;
+  case GOT_SYNC1:
+    if (c != INTERMCU_SYNC2) {
+      goto error;
+    }
+    intermcu_data.ck_a = 0;
+    intermcu_data.ck_b = 0;
+    intermcu_data.status++;
+    break;
+  case GOT_SYNC2:
+    if (intermcu_data.msg_available) {
+      /* Previous message has not yet been parsed: discard this one */
+      goto error;
+    }
+    intermcu_data.msg_class = c;
+    intermcu_data.status++;
+    break;
+  case GOT_CLASS:
+    intermcu_data.msg_id = c;
+    intermcu_data.status++;
+    break;
+  case GOT_ID:
+    intermcu_data.len = c;
+    intermcu_data.status++;
+    break;
+  case GOT_LEN1:
+    intermcu_data.len |= (c<<8);
+    if (intermcu_data.len > INTERMCU_MAX_PAYLOAD) {
+      goto error;
+    }
+    intermcu_data.msg_idx = 0;
+    intermcu_data.status++;
+    break;
+  case GOT_LEN2:
+    intermcu_data.msg_buf[intermcu_data.msg_idx] = c;
+    intermcu_data.msg_idx++;
+    if (intermcu_data.msg_idx >= intermcu_data.len) {
+      intermcu_data.status++;
+    }
+    break;
+  case GOT_PAYLOAD:
+    if (c != intermcu_data.ck_a) {
+      goto error;
+    }
+    intermcu_data.status++;
+    break;
+  case GOT_CHECKSUM1:
+    if (c != intermcu_data.ck_b) {
+      goto error;
+    }
+    intermcu_data.msg_available = TRUE;
+    goto restart;
+    break;
+  default:
+    goto error;
+  }
+  return;
+ error:
+  intermcu_data.error_cnt++;
+ restart:
+  intermcu_data.status = UNINIT;
+  return;
+}
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// USER
 
 
 struct link_mcu_msg link_mcu_from_ap_msg;
@@ -41,48 +230,43 @@ struct link_mcu_msg link_mcu_from_fbw_msg;
 
 inline void parse_mavpilot_msg( void );
 
-#define UbxInitCheksum() { send_ck_a = send_ck_b = 0; }
-#define UpdateChecksum(c) { send_ck_a += c; send_ck_b += send_ck_a; }
-#define UbxTrailer() { GpsUartSend1(send_ck_a);  GpsUartSend1(send_ck_b); }
-
-#define UbxSend1(c) { uint8_t i8=c; GpsUartSend1(i8); UpdateChecksum(i8); }
-#define UbxSend2(c) { uint16_t i16=c; UbxSend1(i16&0xff); UbxSend1(i16 >> 8); }
-#define UbxSend1ByAddr(x) { UbxSend1(*x); }
-#define UbxSend2ByAddr(x) { UbxSend1(*x); UbxSend1(*(x+1)); }
-#define UbxSend4ByAddr(x) { UbxSend1(*x); UbxSend1(*(x+1)); UbxSend1(*(x+2)); UbxSend1(*(x+3)); }
-
-#define UbxHeader(nav_id, msg_id, len) { \
-  GpsUartSend1(UBX_SYNC1); \
-  GpsUartSend1(UBX_SYNC2); \
-  UbxInitCheksum(); \
-  UbxSend1(nav_id); \
-  UbxSend1(msg_id); \
-  UbxSend2(len); \
+void link_mcu_init( void )
+{
+   intermcu_data.status = UNINIT;
+   intermcu_data.msg_available = FALSE;
+   intermcu_data.error_cnt = 0;
 }
-
-
-//static uint8_t  ubx_status;
-//static uint16_t ubx_len;
-//static uint8_t  ubx_msg_idx;
-//static uint8_t ck_a, ck_b;
-uint8_t send_ck_a, send_ck_b;
 
 void parse_mavpilot_msg( void ) 
 {
-//  uint8_t s1;
-
-  if (ubx_class == UBX_MAVPILOT_ID)
+  if (intermcu_data.msg_class == MSG_INTERMCU_ID)
   {
-    if (ubx_id == UBX_MAVPILOT_COMMAND_ID)
+    if (intermcu_data.msg_id == MSG_INTERMCU_COMMAND_ID)
     {
-      ap_state->commands[0] = ((pprz_t)UBX_MAVPILOT_COMMAND_S1(ubx_msg_buf));
-      ap_state->commands[1] = ((pprz_t)UBX_MAVPILOT_COMMAND_S2(ubx_msg_buf));
-      ap_state->commands[2] = ((pprz_t)UBX_MAVPILOT_COMMAND_S3(ubx_msg_buf));
-      ap_state->commands[3] = ((pprz_t)UBX_MAVPILOT_COMMAND_S4(ubx_msg_buf));
-      ap_state->commands[4] = ((pprz_t)UBX_MAVPILOT_COMMAND_S5(ubx_msg_buf));
+#if COMMANDS_NB > 6
+#warning "INTERMCU UART CAN ONLY SEND 6 COMMANDS"
+#endif
+
+#if COMMANDS_NB > 0
+      ap_state->commands[0] = ((pprz_t)MSG_INTERMCU_COMMAND_S1(intermcu_data.msg_buf));
+#endif
+#if COMMANDS_NB > 1
+      ap_state->commands[1] = ((pprz_t)MSG_INTERMCU_COMMAND_S2(intermcu_data.msg_buf));
+#endif
+#if COMMANDS_NB > 2
+      ap_state->commands[2] = ((pprz_t)MSG_INTERMCU_COMMAND_S3(intermcu_data.msg_buf));
+#endif
+#if COMMANDS_NB > 3
+      ap_state->commands[3] = ((pprz_t)MSG_INTERMCU_COMMAND_S4(intermcu_data.msg_buf));
+#endif
+#if COMMANDS_NB > 4
+      ap_state->commands[4] = ((pprz_t)MSG_INTERMCU_COMMAND_S5(intermcu_data.msg_buf));
+#endif
+#if COMMANDS_NB > 5
+      ap_state->commands[5] = ((pprz_t)MSG_INTERMCU_COMMAND_S6(intermcu_data.msg_buf));
+#endif
 
       inter_mcu_received_ap = TRUE;
-      //inter_mcu_received_fbw = TRUE;
     }
   }
 }
@@ -100,37 +284,25 @@ void link_mcu_periodic_task( void )
     SixtyHzCounter = 0;
     inter_mcu_fill_fbw_state(); /** Prepares the next message for AP */
 
-    UbxSend_MAVPILOT_FBW(
-/*	fbw_state->channels[0],
-	fbw_state->channels[1],
-	fbw_state->channels[2],
-	fbw_state->channels[3],
-	fbw_state->channels[4],
-*/	fbw_state->ppm_cpt,
+    InterMcuSend_INTERMCU_FBW(
+	fbw_state->ppm_cpt,
 	fbw_state->status,
 	fbw_state->nb_err,
 	fbw_state->vsupply,
 	fbw_state->current);
-/*
-};
-
-struct ap_state {
-  pprz_t commands[COMMANDS_NB];  
-};
-
-	);
-*/
   }
 }
 
 void link_mcu_event_task( void ) {
   /* A message has been received */
-  if (GpsBuffer()) {
-    ReadGpsBuffer();
+  if (InterMcuBuffer()) {
+    while (InterMcuLink(ChAvailable())&&!intermcu_data.msg_available)
+      intermcu_parse(InterMcuLink(Getch()));
   }
-  if (gps_msg_received) {
+
+  if (intermcu_data.msg_available) {
     parse_mavpilot_msg();
-    gps_msg_received = FALSE;
+    intermcu_data.msg_available = FALSE;
   }
 }
    
