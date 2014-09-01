c5cdd6c5e0e1d554df62cc2313a175764ff0f2af
Odroid XU <odroid@openuas.org>
2014-09-01 09:35:49 +0000

    Simple attempt

--- allthings_obc2014/src/candy/candy.c
+++ allthings_obc2014/src/candy/candy.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "serial.h"
 
 int main(int argc, char* argv[])
 {
+  int i;
   char* buff = "This is a nice demo\n";
+
+  printf("Start Shoot\n");
+
+  system("cd ../popcorn/chdkptp/lua/ && ../chdkptp -c -e'rec' -e'rs'");
+  for (i=0;i<10;i++)
+    system("cd ../popcorn/chdkptp/lua/ && ../chdkptp -c -e'rs'");
+
+
+  printf("Ready\n");
 
   // Open
   serial_init();
