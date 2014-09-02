78ee8c9e4364794459d4a455bde30249b180735d
Odroid XU <odroid@openuas.org>
2014-09-02 22:35:58 +0000

    candy actually received commandsgit status

--- allthings_obc2014/src/candy/candy.c
+++ allthings_obc2014/src/candy/candy.c
@@ -8,20 +8,30 @@ int main(int argc, char* argv[])
   int i;
   char* buff = "This is a nice demo\n";
 
-  printf("Start Shoot\n");
-
-  system("cd ../popcorn/chdkptp/lua/ && ../chdkptp -c -e'rec' -e'rs'");
-  for (i=0;i<10;i++)
-    system("cd ../popcorn/chdkptp/lua/ && ../chdkptp -c -e'rs'");
-
-
-  printf("Ready\n");
+  //printf("Start Shoot\n");
+  //system("cd ../popcorn/chdkptp/lua/ && ../chdkptp -c -e'rec' -e'rs'");
+  //for (i=0;i<10;i++)
+  //  system("cd ../popcorn/chdkptp/lua/ && ../chdkptp -c -e'rs'");
+  //printf("Ready\n");
 
   // Open
   serial_init();
 
   // Send
   write(fd, buff, 10);
+
+  for (;;)
+  {
+    char c;
+    int ret = read(fd, &c, 1);
+    if (ret != -1)
+    {
+      if (c == 0x20)
+        system("cd ../popcorn/chdkptp/lua/ && ../chdkptp -c -e'rec' -e'rs'");
+      printf("%02X,(%d)\n",c, ret);
+    }
+  }
+
 
   // Close
   close(fd);
