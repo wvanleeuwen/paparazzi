a8dddb8e9f64127c1731558585a8faae1f1cb459
dewagter <dewagter@gmail.com>
2014-09-09 11:29:42 +0200

    pressure formula's for qnh #4

--- sw/airborne/modules/altitude/qnh.c
+++ sw/airborne/modules/altitude/qnh.c
@@ -21,11 +21,53 @@
  */
 
 #include "qnh.h"
+#include "state.h"
 #include "subsystems/sensors/baro.h"
+#include "generated/airframe.h"
+
+float qnh = 0;
+float amsl = 0;
+
+#if PERIODIC_TELEMETRY
+#include "subsystems/datalink/telemetry.h"
+
+static void send_amsl(void)
+{
+  DOWNLINK_SEND_AMSL(DefaultChannel, DefaultDevice, &amsl, &qnh);
+}
+#endif
+
 
 void init_qnh(void) {
+#if PERIODIC_TELEMETRY
+  register_periodic_telemetry(&telemetry_Ap, "AMSL", send_amsl);
+#endif
+  qnh = 1013.00;
 }
 
 void periodic_qnh(void) {
+/*
+  1200Pa per 100m
+  8.333cm per Pa
+
+  (1 -   (p/p0) ^ ((R*L)/(g*M))    ) * T0/L
+*/
+
+  const float L = 0.0065; // [K/m]
+  const float p0 = 101325; // [Pa]
+  const float T0 = 288.15; // [K]
+  const float g = 9.80665; // [m/s^2]
+  const float M = 0.0289644; // [kg/mol]
+  const float R = 8.31447; // [J/(mol*K)]
+  const float MeterPerFeet = 0.3048;
+
+  float h = stateGetPositionLla_f()->alt;
+
+  float Trel = 1 - L*h/T0;
+  float Expo = g * M / R / L;
+  float p = p0 * pow(Trel,Expo);
+  qnh = p / 100.0f;
+
+  amsl = h / MeterPerFeet;
 }
 
