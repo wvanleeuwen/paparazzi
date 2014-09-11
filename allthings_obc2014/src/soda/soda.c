97a21f28d46d94bd39917b59debd81ad1e832801
OXU3 <oxu3@openuas.org>
2014-09-11 14:30:46 +0000

    Soda creates output file #22

--- allthings_obc2014/src/soda/soda.c
+++ allthings_obc2014/src/soda/soda.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "jpeg_decode.h"
 #include "rgb2hsv.h"
 
@@ -10,6 +11,7 @@ int main(int argc, char* argv[])
   unsigned char* buff = new unsigned char [4000*3000*3];
   unsigned char* hsv = new unsigned char [4000*3000*3];
 
+  char  outfile[1024];
   printf("Start Superb Onboard Recognition Application\n");
 
   //////////////////////////////////////////////
@@ -40,7 +42,12 @@ int main(int argc, char* argv[])
     q+=3;
   }
 
-  
+  strcpy(outfile,filename);
+  strcat(outfile,".txt");
+
+  FILE* fp = fopen(outfile, "w+b");
+  fprintf(fp, "processed  %s \n", filename);
+  fclose(fp);
 
   printf("Ready!\n");
   return 0;
