0c08e39fed298728f9c700b73a11cad9b03af74b
OXU3 <oxu3@openuas.org>
2014-09-11 12:30:00 +0000

    soda reads popcorn file

--- allthings_obc2014/src/soda/soda.c
+++ allthings_obc2014/src/soda/soda.c
@@ -5,31 +5,42 @@
 
 int main(int argc, char* argv[])
 {
+  char* filename = "/root/IMG_0200.jpg";
+
   unsigned char* buff = new unsigned char [4000*3000*3];
   unsigned char* hsv = new unsigned char [4000*3000*3];
 
   printf("Start Superb Onboard Recognition Application\n");
 
-  loadJpg("/root/IMG_0269.jpg", buff);
+  //////////////////////////////////////////////
+  // Load Jpeg
 
-  printf("JPEG Loaded\n");
-
-  for (int i=0;i<10;i++)
+  if (argc >= 2)
   {
-    printf("%d:\n",i);
-    unsigned char* p = buff;
-    unsigned char* q = hsv;
-    unsigned char* end = buff + 4000*3000*3;
-    //for (int x=0;x<(4000*3000*3);x+=3)
-    for (;p<end;p+=3)
-    {
-      //for (int y=0;y<3000;y++)
-      //{
-        RgbToHsvP( p, q);
-        q+=3;
-      //}
-    }
+    filename = argv[1];
   }
+
+  int ret = loadJpg(filename, buff);
+  if (ret < 0)
+  {
+    fprintf(stderr, "Failed to load '%s'\n",filename);
+    return -1;
+  }
+
+  printf("JPEG '%s' Loaded\n",filename);
+
+  //////////////////////////////////////////////
+  // Convert to HSV
+  unsigned char* p = buff;
+  unsigned char* q = hsv;
+  unsigned char* end = buff + 4000*3000*3;
+  for (;p<end;p+=3)
+  {
+    RgbToHsvP( p, q);
+    q+=3;
+  }
+
+  
 
   printf("Ready!\n");
   return 0;
