60ff08bd23ea2869756fbff13274966a1449fd0a
dewagter <dewagter@gmail.com>
2014-09-12 11:36:32 +0200

    same protocol on both sides of the line

--- allthings_obc2014/src/candy/protocol.h
+++ allthings_obc2014/src/candy/protocol.h
@@ -97,22 +97,20 @@ extern uint8_t mora_ck_a, mora_ck_b;
 #define MoraPutUint8( _byte) {     \
   mora_ck_a += _byte;              \
   mora_ck_b += mora_ck_a;          \
-  write(fd, &_byte, 1);            \
+  CameraLink(Transmit(_byte));     \
 }
 
 #define MoraHeader(msg_id, payload_len) {           \
-  uint8_t s_c = STX;                                \
-  write(fd, &s_c, 1);                               \
-  s_c = MoraSizeOf( payload_len);                   \
-  write(fd, &s_c, 1);                               \
-  mora_ck_a = s_c; mora_ck_b = s_c;                 \
-  s_c = msg_id;                                     \
-  MoraPutUint8(s_c);                                \
+  CameraLink(Transmit(STX));                        \
+  uint8_t msg_len = MoraSizeOf( payload_len);       \
+  CameraLink(Transmit(msg_len));                    \
+  mora_ck_a = msg_len; mora_ck_b = msg_len;         \
+  MoraPutUint8(msg_id);                             \
 }
 
-#define MoraTrailer() {            \
-  write(fd, &mora_ck_a, 1);        \
-  write(fd, &mora_ck_b, 1);        \
+#define MoraTrailer() {               \
+  CameraLink(Transmit(mora_ck_a));    \
+  CameraLink(Transmit(mora_ck_b));    \
 }
 
 #define MoraPut1ByteByAddr( _byte) {  \
