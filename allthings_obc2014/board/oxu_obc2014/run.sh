d14a0b9293f193a393e637458fc12f665b9b1eaa
OXU3 <oxu3@openuas.org>
2014-09-15 12:47:19 +0000

    [OXU] Fix faster boot without eth0 and date in log messages

--- allthings_obc2014/board/oxu_obc2014/run.sh
+++ allthings_obc2014/board/oxu_obc2014/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-screen -mS candy bash -c "cd /root/develop/allthings_obc2014/src/candy; ./candy 2>&1 | tee -a /root/candy.log"
+screen -mS candy bash -c "cd /root/develop/allthings_obc2014/src/candy; ./candy 2>&1 | tee -a /root/candy_`date +%Y-%m-%d_%H:%M`.log"
--- allthings_obc2014/board/oxu_obc2014/uninstall.sh
+++ allthings_obc2014/board/oxu_obc2014/uninstall.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 cp ./local.rc.stop /etc/rc.local
+cp ./failsafe.conf.start /etc/init/failsafe.conf
