63a38517e1ef8dc6682cb0a9143baf0ccdf58d6d
Freek van Tienen <freek.v.tienen@gmail.com>
2015-07-03 14:17:00 +0200

    [picoquad] Start

---
+++ conf/boards/picoquad_1.0.makefile
@@ -0,0 +1,60 @@
+# Hey Emacs, this is a -*- makefile -*-
+#
+# picoquad_1.0.makefile
+#
+# http://wiki.paparazziuav.org/wiki/picoquad_1.0
+#
+
+BOARD=picoquad
+BOARD_VERSION=1.0
+BOARD_CFG=\"boards/$(BOARD)_$(BOARD_VERSION).h\"
+
+ARCH=stm32
+ARCH_L=f4
+HARD_FLOAT=yes
+$(TARGET).ARCHDIR = $(ARCH)
+$(TARGET).LDSCRIPT=$(SRC_ARCH)/lisa-mx.ld
+
+# -----------------------------------------------------------------------
+
+# default flash mode is via usb dfu bootloader (luftboot)
+# other possibilities: DFU-UTIL, SWD, JTAG_BMP, STLINK, SERIAL
+FLASH_MODE ?= SWD
+
+HAS_LUFTBOOT ?= 0
+ifeq (,$(findstring $(HAS_LUFTBOOT),0 FALSE))
+$(TARGET).CFLAGS+=-DLUFTBOOT
+$(TARGET).LDFLAGS+=-Wl,-Ttext=0x8004000
+DFU_ADDR = 0x8004000
+DFU_PRODUCT = Lisa/Lia
+endif
+
+
+#
+# default LED configuration
+#
+RADIO_CONTROL_LED  ?= none
+BARO_LED           ?= none
+AHRS_ALIGNER_LED   ?= none
+GPS_LED            ?= none
+SYS_TIME_LED       ?= none
+
+
+#
+# default uart configuration
+#
+#RADIO_CONTROL_SPEKTRUM_PRIMARY_PORT   ?= UART1
+
+MODEM_PORT ?= UART2
+MODEM_BAUD ?= B57600
+
+
+#
+# default actuator configuration
+#
+# you can use different actuators by adding a configure option to your firmware section
+# e.g. <configure name="ACTUATORS" value="actuators_ppm/>
+# and by setting the correct "driver" attribute in servo section
+# e.g. <servo driver="Ppm">
+#
+ACTUATORS ?= actuators_pwm
---
+++ sw/airborne/boards/picoquad_1.0.h
@@ -0,0 +1,185 @@
+/*
+ * Copyright (C) 2016 Freek van Tienen <freek.v.tienen@gmail.com>
+ *
+ * This file is part of paparazzi.
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+#ifndef CONFIG_PICOQUAD_H
+#define CONFIG_PICOQUAD_H
+
+#define BOARD_PICOQUAD
+
+/* Lisa/M has a 12MHz external clock and 168MHz internal. */
+#define EXT_CLK 12000000
+#define AHB_CLK 168000000
+
+/*
+ * Onboard LEDs
+ */
+#ifndef USE_LED_1
+#define USE_LED_1 1
+#endif
+#define LED_1_GPIO GPIOE
+#define LED_1_GPIO_PIN GPIO11
+#define LED_1_GPIO_ON gpio_clear
+#define LED_1_GPIO_OFF gpio_set
+#define LED_1_AFIO_REMAP ((void)0)
+
+/* Default actuators driver */
+#define DEFAULT_ACTUATORS "subsystems/actuators/actuators_pwm.h"
+#define ActuatorDefaultSet(_x,_y) ActuatorPwmSet(_x,_y)
+#define ActuatorsDefaultInit() ActuatorsPwmInit()
+#define ActuatorsDefaultCommit() ActuatorsPwmCommit()
+
+
+/* UART */
+#define UART2_GPIO_AF GPIO_AF7
+#define UART2_GPIO_PORT_RX GPIOA
+#define UART2_GPIO_RX GPIO3
+#define UART2_GPIO_PORT_TX GPIOA
+#define UART2_GPIO_TX GPIO2
+
+/*
+ * Spektrum
+ */
+/* The line that is pulled low at power up to initiate the bind process */
+/* These are not common between versions of lisa/mx and thus defined in the
+ * version specific header files. */
+/* #define SPEKTRUM_BIND_PIN GPIO0 */
+/* #define SPEKTRUM_BIND_PIN_PORT GPIOB */
+
+/*#define SPEKTRUM_UART1_RCC RCC_USART1
+#define SPEKTRUM_UART1_BANK GPIOA
+#define SPEKTRUM_UART1_PIN GPIO10
+#define SPEKTRUM_UART1_AF GPIO_AF7
+#define SPEKTRUM_UART1_IRQ NVIC_USART1_IRQ
+#define SPEKTRUM_UART1_ISR usart1_isr
+#define SPEKTRUM_UART1_DEV USART1
+
+#define SPEKTRUM_UART5_RCC RCC_UART5
+#define SPEKTRUM_UART5_BANK GPIOD
+#define SPEKTRUM_UART5_PIN GPIO2
+#define SPEKTRUM_UART5_AF GPIO_AF8
+#define SPEKTRUM_UART5_IRQ NVIC_UART5_IRQ
+#define SPEKTRUM_UART5_ISR uart5_isr
+#define SPEKTRUM_UART5_DEV UART5*/
+
+/* PPM
+ *
+ * Default is PPM config 2, input on GPIOA1 (Servo pin 6)
+ */
+
+/* SPI */
+#define SPI2_GPIO_AF GPIO_AF5
+#define SPI2_GPIO_PORT_MISO GPIOB
+#define SPI2_GPIO_MISO GPIO14
+#define SPI2_GPIO_PORT_MOSI GPIOB
+#define SPI2_GPIO_MOSI GPIO15
+#define SPI2_GPIO_PORT_SCK GPIOB
+#define SPI2_GPIO_SCK GPIO13
+
+#define SPI_SELECT_SLAVE0_PORT GPIOB
+#define SPI_SELECT_SLAVE0_PIN GPIO12
+
+
+/* I2C mapping */
+#define I2C2_GPIO_PORT GPIOB
+#define I2C2_GPIO_SCL GPIO10
+#define I2C2_GPIO_SDA GPIO11
+
+
+/*
+ * ADC
+ */
+
+
+
+/*
+ * PWM
+ *
+ */
+#define PWM_USE_TIM1 1
+#define PWM_USE_TIM5 1
+
+#define USE_PWM1 1
+#define USE_PWM2 1
+#define USE_PWM3 1
+#define USE_PWM4 1
+
+// PWM_SERVO_x is the index of the servo in the actuators_pwm_values array
+#if USE_PWM1
+#define PWM_SERVO_1 0
+#define PWM_SERVO_1_TIMER TIM1
+#define PWM_SERVO_1_GPIO GPIOE
+#define PWM_SERVO_1_PIN GPIO11
+#define PWM_SERVO_1_AF GPIO_AF1
+#define PWM_SERVO_1_OC TIM_OC2
+#define PWM_SERVO_1_OC_BIT (1<<1)
+#else
+#define PWM_SERVO_1_OC_BIT 0
+#endif
+
+#if USE_PWM2
+#define PWM_SERVO_2 1
+#define PWM_SERVO_2_TIMER TIM1
+#define PWM_SERVO_2_GPIO GPIOE
+#define PWM_SERVO_2_PIN GPIO9
+#define PWM_SERVO_2_AF GPIO_AF1
+#define PWM_SERVO_2_OC TIM_OC1
+#define PWM_SERVO_2_OC_BIT (1<<0)
+#else
+#define PWM_SERVO_2_OC_BIT 0
+#endif
+
+#if USE_PWM3
+#define PWM_SERVO_3 2
+#define PWM_SERVO_3_TIMER TIM5
+#define PWM_SERVO_3_GPIO GPIOA
+#define PWM_SERVO_3_PIN GPIO0
+#define PWM_SERVO_3_AF GPIO_AF2
+#define PWM_SERVO_3_OC TIM_OC1
+#define PWM_SERVO_3_OC_BIT (1<<0)
+#else
+#define PWM_SERVO_3_OC_BIT 0
+#endif
+
+#if USE_PWM4
+#define PWM_SERVO_4 3
+#define PWM_SERVO_4_TIMER TIM1
+#define PWM_SERVO_4_GPIO GPIOE
+#define PWM_SERVO_4_PIN GPIO13
+#define PWM_SERVO_4_AF GPIO_AF2
+#define PWM_SERVO_4_OC TIM_OC3
+#define PWM_SERVO_4_OC_BIT (1<<2)
+#else
+#define PWM_SERVO_4_OC_BIT 0
+#endif
+
+/* servos 1,2,4 on TIM3 */
+#define PWM_TIM1_CHAN_MASK (PWM_SERVO_1_OC_BIT|PWM_SERVO_2_OC_BIT|PWM_SERVO_4_OC_BIT)
+/* servo 3 on TIM5 */
+#define PWM_TIM5_CHAN_MASK (PWM_SERVO_3_OC_BIT)
+
+
+/* by default activate onboard baro */
+/*#ifndef USE_BARO_BOARD
+#define USE_BARO_BOARD 1
+#endif*/
+
+#endif /* CONFIG_PICOQUAD_H */
