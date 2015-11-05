e6efc035f51101211e8e111a574556fffb06260c
Christophe De Wagter <dewagter@gmail.com>
2015-11-05 20:31:28 +0100

    helicopter

---
+++ sw/airborne/modules/helicopter/throttle_curve.c
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) C. De Wagter
+ *
+ * This file is part of paparazzi
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+/**
+ * @file "modules/helicopter/throttle_curve.c"
+ * @author C. De Wagter
+ * Throttle Curve Mixers
+ */
+
+#include "modules/helicopter/throttle_curve.h"
+
+void throttle_curve_init(void) {}
+void throttle_curve_periodic(void) {}
+
+
