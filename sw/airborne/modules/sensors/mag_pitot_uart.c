10cca1c8f268125f7332a0f4b7c75e246c4b160b
Freek van Tienen <freek.v.tienen@gmail.com>
2016-04-10 14:27:12 +0200

    [modules] Add magneto rotation to remote Mag Pitot

--- sw/airborne/modules/sensors/mag_pitot_uart.c
+++ sw/airborne/modules/sensors/mag_pitot_uart.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) C. De Wagter
+ * Copyright (C) 2015 Freek van Tienen <freek.v.tienen@gmail.com>
  *
  * This file is part of paparazzi
  *
@@ -28,25 +29,16 @@
 #include "pprzlink/pprz_transport.h"
 #include "pprzlink/intermcu_msg.h"
 #include "mcu_periph/uart.h"
-
 #include "subsystems/abi.h"
 #include "subsystems/imu.h"
 
+/* Main magneto structure */
+static struct mag_pitot_t mag_pitot = {
+  .device = (&((MAG_PITOT_PORT).device)),
+  .msg_available = FALSE
+};
+static uint8_t mp_msg_buf[128]  __attribute__((aligned));   ///< The message buffer for the Magneto and pitot
 
-// Variables
-static struct link_device *mag_pitot_device = (&((MAG_PITOT_PORT).device));
-static struct pprz_transport mag_pitot_transport;
-static uint8_t mag_pitot_buf[128]  __attribute__((aligned));
-static bool_t mag_pitot_msg_available = FALSE;
-
-
-
-
-// Downlink
-#include "mcu_periph/uart.h"
-#include "pprzlink/messages.h"
-#include "subsystems/datalink/downlink.h"
-#include BOARD_CONFIG
 
 #if PERIODIC_TELEMETRY
 #include "subsystems/datalink/telemetry.h"
@@ -58,69 +50,76 @@ static void mag_pitot_raw_downlink(struct transport_tx *trans, struct link_devic
 }
 #endif
 
-
+/* Initialize the magneto and pitot */
 void mag_pitot_init() {
-  pprz_transport_init(&mag_pitot_transport);
+  // Initialize transport protocol
+  pprz_transport_init(&mag_pitot.transport);
+
+  // Set the Imu to Magneto rotation
+  struct FloatEulers imu_to_mag_eulers =
+    {IMU_TO_MAG_PHI, IMU_TO_MAG_THETA, IMU_TO_MAG_PSI};
+  orientationSetEulers_f(&mag_pitot.imu_to_mag, &imu_to_mag_eulers);
 
 #if PERIODIC_TELEMETRY
   register_periodic_telemetry(DefaultPeriodic, PPRZ_MSG_ID_IMU_MAG_RAW, mag_pitot_raw_downlink);
 #endif
-
 }
 
-
+/* Parse the InterMCU message */
 static inline void mag_pitot_parse_msg(void)
 {
   uint32_t now_ts = get_sys_time_usec();
 
   /* Parse the mag-pitot message */
-  uint8_t msg_id = mag_pitot_buf[1];
+  uint8_t msg_id = mp_msg_buf[1];
   switch (msg_id) {
+
+  /* Got a magneto message */
   case DL_IMCU_REMOTE_MAG: {
-    struct Int16Vect3 mag;
+    struct Int32Vect3 raw_mag;
 
-    mag.x = DL_IMCU_REMOTE_MAG_mag_x(mag_pitot_buf);
-    mag.y = DL_IMCU_REMOTE_MAG_mag_y(mag_pitot_buf);
-    mag.z = DL_IMCU_REMOTE_MAG_mag_z(mag_pitot_buf);
+    // Read the raw magneto information
+    raw_mag.x = DL_IMCU_REMOTE_MAG_mag_x(mp_msg_buf);
+    raw_mag.y = DL_IMCU_REMOTE_MAG_mag_y(mp_msg_buf);
+    raw_mag.z = DL_IMCU_REMOTE_MAG_mag_z(mp_msg_buf);
 
-    // TODO: Rotate from Mag Axis to IMU Axis
-    VECT3_COPY(imu.mag_unscaled, mag);
+    // Rotate the magneto
+    struct Int32RMat *imu_to_mag_rmat = orientationGetRMat_i(&mag_pitot.imu_to_mag);
+    int32_rmat_vmult(&imu.mag_unscaled, imu_to_mag_rmat, &raw_mag);
 
+    // Send and set the magneto IMU data
     imu_scale_mag(&imu);
-    AbiSendMsgIMU_MAG_INT32(IMU_BOARD_ID, now_ts, &imu.mag);
-
+    AbiSendMsgIMU_MAG_INT32(IMU_MAG_PITOT_ID, now_ts, &imu.mag);
     break;
   }
 
+  /* Got a barometer message */
   case DL_IMCU_REMOTE_BARO: {
-    float abs = 1.0f * ((float)(DL_IMCU_REMOTE_BARO_pitot_tot(mag_pitot_buf)));
-    float diff = 1.0f * DL_IMCU_REMOTE_BARO_pitot_stat(mag_pitot_buf);
-    float temp = 1.0f * DL_IMCU_REMOTE_BARO_pitot_temp(mag_pitot_buf);
+    float baro_abs = 1.0f * ((float)(DL_IMCU_REMOTE_BARO_pitot_tot(mp_msg_buf)));
+    float baro_diff = 1.0f * DL_IMCU_REMOTE_BARO_pitot_stat(mp_msg_buf);
+    float baro_temp = 1.0f * DL_IMCU_REMOTE_BARO_pitot_temp(mp_msg_buf);
 
-    AbiSendMsgBARO_ABS(BARO_BOARD_SENDER_ID, abs);
-    AbiSendMsgBARO_DIFF(BARO_BOARD_SENDER_ID, diff);
-    AbiSendMsgTEMPERATURE(BARO_BOARD_SENDER_ID, temp);
+    AbiSendMsgBARO_ABS(IMU_MAG_PITOT_ID, baro_abs);
+    AbiSendMsgBARO_DIFF(IMU_MAG_PITOT_ID, baro_diff);
+    AbiSendMsgTEMPERATURE(IMU_MAG_PITOT_ID, baro_temp);
     break;
   }
 
     default:
       break;
   }
-
-
 }
 
-
-
+/* We need to wait for incomming messages */
 void mag_pitot_event() {
+  // Check if we got some message from the Magneto or Pitot
+  pprz_check_and_parse(mag_pitot.device, &mag_pitot.transport, mp_msg_buf, &mag_pitot.msg_available);
 
-  pprz_check_and_parse(mag_pitot_device, &mag_pitot_transport, mag_pitot_buf, &mag_pitot_msg_available );
-
-  if (mag_pitot_msg_available) {
+  // If we have a message we should parse it
+  if (mag_pitot.msg_available) {
     mag_pitot_parse_msg();
-    mag_pitot_msg_available = FALSE;
+    mag_pitot.msg_available = FALSE;
   }
-
 }
 
 
