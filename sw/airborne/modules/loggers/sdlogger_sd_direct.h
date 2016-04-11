a60999c19d29b92c5d9d2976c47e76c5ba70104f
Christophe De Wagter <dewagter@gmail.com>
2016-04-11 12:07:27 +0200

    Old-style logfile

---
+++ sw/airborne/modules/loggers/sdlogger_sd_direct.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) C. De Wagter
+ *
+ * This file is part of paparazzi
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+/**
+ * @file "modules/loggers/sdlogger_sd_direct.h"
+ * @author C. De Wagter
+ * Log spi direct variables direct on SD card
+ */
+
+#ifndef SDLOGGER_SD_DIRECT_H
+#define SDLOGGER_SD_DIRECT_H
+
+// extern void sdlogger_direct_init();
+// extern void sdlogger_direct_periodic();
+
+#endif
+
