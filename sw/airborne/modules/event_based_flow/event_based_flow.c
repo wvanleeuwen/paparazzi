b4e4a4d6f096ad161e961e9d2a00c1beea9209dc
baspijhor <baspijnackerh@gmail.com>
2016-07-01 11:08:48 +0200

    [event_based_flow] Created empty module

---
+++ sw/airborne/modules/event_based_flow/event_based_flow.c
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) baspijhor
+ *
+ * This file is part of paparazzi
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+/**
+ * @file "modules/event_based_flow/event_based_flow.c"
+ * @author baspijhor
+ * 
+ */
+
+#include "modules/event_based_flow/event_based_flow.h"
+
+void event_based_flow_init() {}
+void event_based_flow_start() {}
+void event_based_flow_periodic() {}
+void event_based_flow_stop() {}
+
