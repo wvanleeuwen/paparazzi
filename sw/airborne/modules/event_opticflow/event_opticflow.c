b6441958fef67be45b58f27470ace4a500acb559
kirkscheper <kirkscheper@gmail.com>
2016-07-11 14:18:22 +0200

    initial commit for dvs over uart

---
+++ sw/airborne/modules/event_opticflow/event_opticflow.c
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) Bas Pijnacker Hordijk
+ *
+ * This file is part of paparazzi
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+/**
+ * @file "modules/event_opticflow/event_opticflow.c"
+ * @author Bas Pijnacker Hordijk
+ * Event based opticflow using DVS camera
+ */
+
+#include "modules/event_opticflow/event_opticflow.h"
+
+#include "mcu_periph/uart.h"
+#include "subsystems/datalink/downlink.h"
+
+#ifndef DVS_PORT
+#error Please define uart port connected to the dvs event based camera. e.g <define name="DVS_PORT" value="uart0"/>
+#endif
+
+void event_opticflow_init(void) {
+
+}
+
+void event_opticflow_periodic(void) {
+  /* read all data from uart */
+  static uint8_t buffer[UART_RX_BUFFER_SIZE];
+  static uint16_t buf_loc = 0;
+
+  while(uart_char_available(&DVS_PORT))
+  {
+    buffer[buf_loc] = uart_getch(&DVS_PORT);
+    buf_loc = (buf_loc + 1) % UART_RX_BUFFER_SIZE;
+  }
+  DOWNLINK_SEND_DEBUG(DefaultChannel, DefaultDevice, 128, buffer);
+}
---
+++ sw/airborne/modules/event_opticflow/event_opticflow.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) Bas Pijnacker Hordijk
+ *
+ * This file is part of paparazzi
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+/**
+ * @file "modules/event_opticflow/event_opticflow.h"
+ * @author Bas Pijnacker Hordijk
+ * Event based opticflow using DVS camera
+ */
+
+#ifndef EVENT_OPTICFLOW_H
+#define EVENT_OPTICFLOW_H
+
+extern void event_opticflow_init(void);
+extern void event_opticflow_periodic(void);
+
+#endif
+
